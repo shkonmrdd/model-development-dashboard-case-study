c0bc66c33cc2819f12a5a22ced609f4504190f10
shkonmrdd <54889360+shkonmrdd@users.noreply.github.com>
2026-01-07 23:30:52 +0300

    Eslint fixes

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -606,9 +606,11 @@ function SidebarMenuSkeleton({
 }: React.ComponentProps<"div"> & {
   showIcon?: boolean
 }) {
-  // Random width between 50 to 90%.
-  const width = React.useMemo(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`
+  const [width, setWidth] = React.useState("70%")
+
+  React.useEffect(() => {
+    const nextWidth = `${Math.floor(Math.random() * 40) + 50}%`
+    setWidth(nextWidth)
   }, [])
 
   return (
